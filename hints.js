0e7759c46157d36764ffa16d45ad7645b40eee47
Somya Lalwani <70585477+somyalalwani@users.noreply.github.com>
2020-11-02 18:19:44 +0530

    hints for simulation
    This is js code for making the code more intercative, further changes will be made to it in future.

---
+++ hints.js
@@ -0,0 +1,55 @@
+function h1p1()
+{
+    alert("All the nouns used in the requirements could be actors. ");
+}
+
+function h1p2()
+{
+    alert("In our case, the possible candidates for being actors could be: Group,Passenger, Passenger with special needs,");
+}
+
+function h1p3()
+{
+    alert("Some of the actors that you could have missed out are : Individual, Passenger, Booking clerk");
+}
+
+function h2p1()
+{
+    alert("If you look at the actors list, you can see that there are multiple types of passengers.")
+}
+function h2p2()
+{
+    alert("This means that an actor namely Passengers has related actors. ");
+}
+
+function h3p1()
+{
+    alert("Once we have identified actors, we can focus on the interactions of the actors with the system.")
+}
+function h3p2()
+{
+    alert("In our case of airline system, some of them which can be identfied as:Check in,Validate pnr,Check weight");
+}
+function h3p3()
+{
+    alert("Some other use cases that you might have missed out are : Calculate overweight charges,Print Boarding pass");
+}
+
+function hint1()
+{ 
+h1p1();
+h1p2();
+h1p3();
+}
+
+function hint2()
+{
+    h2p1();
+    h2p2();
+}
+function hint3()
+{
+    h3p1();
+    h3p2();
+    h3p3();    
+}
