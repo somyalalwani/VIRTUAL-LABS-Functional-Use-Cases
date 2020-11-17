00496740c0b5d8a0433ce8cd2cbacbaad990959a
aayushinigam <aayushinigam2109@gmail.com>
2020-11-17 11:39:47 +0530

    dragndrop final

---
+++ hints2.js
@@ -0,0 +1,33 @@
+function hint1()
+{
+    alert("\u2022 All the nouns used in the requirements could be actors.\n\u2022 In our case, the possible candidates for being actors could be: Group, Passenger, Passenger with special needs, etc.\n\u2022 Some of the actors that you could have missed out are : Individual, Passenger, Booking clerk");
+}
+
+function hint2()
+{
+    alert("\u2022 If you look at the actors list, you can see that there are multiple types of passengers.\n\u2022 This means that an actor namely Passengers has related actors.");
+}
+
+function hint3()
+{
+    alert("\u2022 Once we have identified actors, we can focus on the interactions of the actors with the system.\n\u2022 In our case of airline system, some of them which can be identfied as: Check in, Validate pnr, Check weight.\n\u2022 Some other use cases that you might have missed out are : Calculate overweight charges, Print Boarding pass");
+}
+
+/*function hint1()
+{ 
+h1p1();
+h1p2();
+h1p3();
+}
+
+function hint2()
+{
+    h2p1();
+    h2p2();
+}
+function hint3()
+{
+    h3p1();
+    h3p2();
+    h3p3();    
+}*/
