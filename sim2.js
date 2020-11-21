b68b232f28c07b71ffd24d01137b60aaae8ec608
aayushinigam <aayushinigam2109@gmail.com>
2020-11-21 18:38:43 +0530

    simulation part2 done

--- sim2.js
+++ sim2.js
@@ -1,27 +1,12 @@
 var use_cases=["Order Food","Serve Food","Eat Food","pay for food","cook food","order wine"];
 var user_use_cases=[];
-function check_use_cases(y) 
-{  
-    var x=y.toLowerCase();
-    if(arraysEqual(use_cases,user_use_cases)) <!--When all possible actors are checked it notifies the user -->
-    {
-        alert("You have entereed all possible use cases!"); 
-        return; 
-    }
-    else
-    {
-        if(use_cases.includes(x))
-        {
-            alert("Correct");
-            if(!user_use_cases.includes(x))
-                {user_use_cases.push(x);}
-        }
-        else 
-        {
-            alert("Try again");
-        }
-    }      
-}
+var usecasesForDiag = new Array();
+var actorsForDiag = new Array();
+var relDict = [];
+var actorNodeDataArray = new Array();
+var usecaseNodeDataArray = new Array();
+var linkDataArray = new Array();
+
 
 function arraysEqual(a, b) 
 {
@@ -38,80 +23,71 @@ function arraysEqual(a, b)
     return true;
 }
 
+
+//helper functions for drag and drop functionality
 function dragStart(ev) {
     ev.dataTransfer.effectAllowed = 'move';
     ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
     ev.dataTransfer.setDragImage(ev.target,0,0);
     return true;
 }
+
 function dragEnter(ev) {
     event.preventDefault();
     return true;
 }
+
 function dragOver(ev) {
     return false;
 }
+
 function dragDrop1(ev) {
     ev.preventDefault();
     var src = ev.dataTransfer.getData("Text");
-    
-    //console.log(typeof(document.getElementById(src)));
-    //console.log(typeof(document.getElementById('actor1')));
-    //document.getElementById(src).innerHTMl = document.getElementById(idd).value;
     ev.target.appendChild(document.getElementById(src));
     document.getElementById('actor1').innerHTML = document.getElementById(src).innerHTML;
     ev.stopPropagation();
     return false;
 }
+
 function dragDrop2(ev) {
     ev.preventDefault();
     var src = ev.dataTransfer.getData("Text");
-    
-    //console.log(typeof(document.getElementById(src)));
-    //console.log(typeof(document.getElementById('actor1')));
-    //document.getElementById(src).innerHTMl = document.getElementById(idd).value;
     ev.target.appendChild(document.getElementById(src));
     document.getElementById('actor2').innerHTML = document.getElementById(src).innerHTML;
     ev.stopPropagation();
     return false;
 }
+
 function dragDrop3(ev) {
     ev.preventDefault();
     var src = ev.dataTransfer.getData("Text");
-    
-    //console.log(typeof(document.getElementById(src)));
-    //console.log(typeof(document.getElementById('actor1')));
-    //document.getElementById(src).innerHTMl = document.getElementById(idd).value;
     ev.target.appendChild(document.getElementById(src));
     document.getElementById('actor3').innerHTML = document.getElementById(src).innerHTML;
     ev.stopPropagation();
     return false;
 }
+
 function dragDrop4(ev) {
     ev.preventDefault();
     var src = ev.dataTransfer.getData("Text");
-    
-    //console.log(typeof(document.getElementById(src)));
-    //console.log(typeof(document.getElementById('actor1')));
-    //document.getElementById(src).innerHTMl = document.getElementById(idd).value;
     ev.target.appendChild(document.getElementById(src));
     document.getElementById('actor4').innerHTML = document.getElementById(src).innerHTML;
     ev.stopPropagation();
     return false;
 }
 
+//check if the user has done it right 
 var actors=["cashier","chef","customer","waiter"];
 
 function check_actors()
 {
-    var adnd=document.getElementById('ansdragndrop').getElementsByTagName('p');
-    var user_actors=[];
-    for(var i=0;i<adnd.length;i++)
+    var adnd = document.getElementById('ansdragndrop').getElementsByTagName('p');
+    var user_actors = [];
+    for(var i = 0;i < adnd.length;i++)
         user_actors.push(adnd.item(i).innerHTML.toLowerCase());
 
-    console.log(user_actors);
-
-    if(user_actors.length!=4)
+    if(user_actors.length != 4)
     {
         alert("Wrong Answer! Less options selected.")
         window.location.reload();
@@ -128,4 +104,96 @@ function check_actors()
             window.location.reload();
         }
     }
+}
+
+var linkDataArray=new Array();
+//draw the diagram 
+function init()
+{
+    var $ = go.GraphObject.make;
+    myDiagram =  
+        $(go.Diagram, "myDiagramDiv", 
+            {
+                layout: $(TableLayout,
+                $(go.RowColumnDefinition, { column: 1, width: 300}))
+            });
+    var i;
+    for(i = 0; i < actorsForDiag.length; i++) {
+        actorNodeDataArray.push(
+            { key: actorsForDiag[i], category: "actor", text:actorsForDiag[i], img:"actor.png", col:0,row:2*i+1}
+        );
+    }
+    for(i = 0; i < usecasesForDiag.length; i++) {
+        usecaseNodeDataArray.push(
+            { key: usecasesForDiag[i], category: "usecase", text:usecasesForDiag[i], col:1,row:i}
+        );
+    }
+
+    var nodeDataArray = actorNodeDataArray.concat(usecaseNodeDataArray);
+
+    myDiagram.model = new go.GraphLinksModel(nodeDataArray,linkDataArray);
+
+    var nodeTemplate = $(go.Node,'Auto',
+                        new go.Binding("location", "loc", go.Point.parse),
+                        $(go.Shape,"Ellipse",{fill:"lightgreen"}),
+                        $(go.TextBlock,"text",{margin:10}, new go.Binding("text","text")),
+                        {margin:10},
+                        new go.Binding("row"),
+                        new go.Binding("column", "col")
+                        );
+                           
+    myDiagram.linkTemplate =
+        $(go.Link, go.Link.Bezier, { reshapable: true }, 
+            $(go.Shape, new go.Binding("strokeDashArray","dash")),
+            $(go.Shape, {toArrow: "Standard", fill: "black"}),
+            $(go.TextBlock, new go.Binding("text", "text"))
+        );
+
+    var actorTemplate = $(go.Node, "Vertical",
+            $(go.Picture,{ maxSize: new go.Size(60, 100) },
+            new go.Binding("source", "img")),
+            $(go.TextBlock, new go.Binding("text", "text")),
+            {margin:10},
+            new go.Binding("row"),
+            new go.Binding("column", "col")
+    );
+
+    var tempmap = new go.Map();
+    tempmap.add("actor", actorTemplate);
+    tempmap.add("usecase", nodeTemplate);
+    myDiagram.nodeTemplateMap = tempmap;
+
+}
+
+//add usecases to its array
+function add_usecase(usecase) {
+    var x = usecase.toLowerCase();
+    usecasesForDiag.push(x);
+    document.getElementById('usecase_diag').value = '';
+    confirm("usecase added");
+
+}
+
+//add actors to its array
+function add_actor(actor) {
+    var x = actor.toLowerCase();
+    actorsForDiag.push(x);
+    document.getElementById('actor_diag').value = '';
+    confirm("actor added");
+}
+
+//add relationship to its array
+function add_relationship(_from,_to,_rel) {
+    _rel="<<"+_rel+">>";
+    if(actorsForDiag.includes(_from)||usecasesForDiag.includes(_from))
+    {
+        if(actorsForDiag.includes(_to)||usecasesForDiag.includes(_to))
+        {
+            if(actorsForDiag.includes(_from)||actorsForDiag.includes(_to)||_rel=="normal")
+                linkDataArray.push({from:_from, to:_to});
+            else
+                linkDataArray.push({from:_from, to:_to, text:_rel, dash:[5,2]});
+        }
+    }
+    alert("Relationship added!");
 }
